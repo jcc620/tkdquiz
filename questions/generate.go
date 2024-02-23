8f3031429162313b8e09dc38f19cbc4471c1d734
Jamie Cronin <jcronin02@gmail.com>
2024-02-23 00:50:06 +0000

    Implement GenerateQuestions

--- questions/generate.go
+++ questions/generate.go
@@ -1,8 +1,22 @@
 package questions
 
-import "github.com/jcc620/tkdquiz/grades"
+import (
+	"math/rand"
+
+	"github.com/jcc620/tkdquiz/grades"
+)
 
 // GenerateQuestions returns qCount questions appropriate for those up to and including grade.
-func GenerateQuestions(grade grades.Grade, qCount int) []Question {
-	return questionBank
+func GenerateQuestions(grade grades.Grade, qCount int, seed int64) []Question {
+	rand.Seed(seed)
+	chosenQuestions := map[int]bool{}
+	questions := []Question{}
+	for len(questions) < qCount {
+		nextQIdx := rand.Intn(len(questionBank))
+		nextQ := questionBank[nextQIdx]
+		if !chosenQuestions[nextQIdx] && nextQ.grade <= grade {
+			questions = append(questions, nextQ)
+		}
+	}
+	return questions
 }
