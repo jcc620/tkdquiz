4f35ba658a279d977fb1e5ccce2a4971ae1078c0
Jamie Cronin <jcronin02@gmail.com>
2024-02-23 00:21:17 +0000

    Add doc comments to questions package

--- questions/question.go
+++ questions/question.go
@@ -1,12 +1,14 @@
+// Package questions implements functionality for generating a set of questions.
 package questions
 
 import (
 	"github.com/jcc620/tkdquiz/grades"
 )
 
+// A Question is a quiz question.
 type Question struct {
-	Query  string
-	Answer string
+	Query  string // the actual question.
+	Answer string // the answer to the question.
 	grade  grades.Grade
 	qType  questionType
 }
